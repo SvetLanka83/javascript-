dca4f726ad34c58824affa66cbf37cf0aaed3a22
SvetLanka83 <svetulichka@gmail.com>
2025-04-22 17:13:34 +0300

    lesson1

---
+++ lesson1/hw1/main.js
@@ -0,0 +1,28 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+let firstName = 'Світлана';
+let middleName = 'Юріївна';
+let lastName = 'Краснопольська';
+let msg = firstName + ' ' + middleName + ' ' + lastName;
+console.log(msg);
+let n = 100;
+let o  = '100';
+let p = true;
+console.log(typeof n );
+console.log(typeof o);
+console.log(typeof p);
+
+
+
